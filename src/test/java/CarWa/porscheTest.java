32d3b57046e1c90c816b1a67afc08ff25065fd57
vaibhav <vibhukatkar96@gmail.com>
2019-09-10 13:20:43 +0530

    cloning

---
+++ src/test/java/CarWa/porscheTest.java
@@ -0,0 +1,19 @@
+package CarWa;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class porscheTest {
+
+    @Test
+
+
+public void shouldCloneStringArray(){
+        String[] array={"one","two","three"};
+        String[] copiedArray=array.clone();
+        assertSame(array,array);
+        assertNotSame(array,copiedArray);
+
+    }
+
+}
